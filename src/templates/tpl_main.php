30557c341aff7e17be148a840286496b6ba92986
Pedro Coelho <up201806802@fe.up.pt>
2020-12-07 02:15:29 +0000

    Added title

--- src/templates/tpl_main.php
+++ src/templates/tpl_main.php
@@ -53,28 +53,31 @@ function draw_aside(){?>
 
 <?php
 function draw_animal_profiles(){?>
-    <div id="animal_profiles">
+    <div class=pets_display>
+        <p class="title_pets_display">Pets available for adoption</p>
+        <div id="animal_profiles">
         <?php
         $animals_array = getAnimals(null,null,null,null,null,null,null,0,20);
         foreach ($animals_array as $animal){
             draw_animal($animal["petId"],$animal["name"],null,$animal["size"],$animal["color"],$animal["location"],null,$animal["user"]);
         }
         ?>
+        </div>
     </div>
+    
 <?php }
+
+
 
 function draw_animal($pet_id,$name,$species,$size,$color,$location,$state,$user){
     ?>
     <a class="animal_main_page" href = "animal_profile.php?pet_id=<?=$pet_id?>"  >
         <div class="animal_img_box">
-            <img src="<?=get_animal_photo($pet_id)?>" width="200" height="200">
-            <div class="animal_text_box">
-                <label>
-                    <?=$name?>
-                </label>
-            </div>
+            <img class= "animal_image_box" src="<?=get_animal_photo($pet_id)?>" width="200" height="200">
+            <label class="animal_text_box">
+                <?=$name?>
+            </label>
         </div>
-        
     </a>
     <?php
 }
