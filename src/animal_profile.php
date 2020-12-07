75b8c3ee2a50e4d7679922145360dfad3a41d8f8
Nunation <oliveiranunopt@gmail.com>
2020-12-07 15:57:40 +0000

    fixed session start error

--- src/animal_profile.php
+++ src/animal_profile.php
@@ -3,7 +3,7 @@ include_once('templates/tpl_common.php');
 include_once('templates/tpl_animal_profile.php');
 include_once('database/animal_queries.php');
 
-session_start();
+//session_start();
 
 if(isset($_GET['pet_id']) && check_pet($_GET['pet_id'])) {
 
