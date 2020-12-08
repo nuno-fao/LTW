c1e6ed1f2708eb4d18d34d427844434a316d9b3d
rocas777 <lmiguelapinto@gmail.com>
2020-12-08 13:30:21 +0000

    added cascade to foreign keys uodate and delete

--- src/templates/tpl_user_profile.php
+++ src/templates/tpl_user_profile.php
@@ -1,47 +1,54 @@
 <?php
-    include_once '../templates/tpl_main.php';
-    function draw_user_aside($user){
-        $user_info =  getUser($user);
-        ?>
-        <aside id="user_profile">
-            <img src = "<?=getPicturePath($user)?>" width="200" height="200"/>
-            <label>
-                <?=$user?>
-            </label>
-            <label>
-                <?=$user_info['Name']?>
-            </label>
-            <label>
-                <?=$user_info['EmailAddress']?>
-            </label>
-        </aside>
-
-        <div>
-            <label>User Animals</label>
-        </div>
-        <section id="user_animals">
-            <?php
-            $animals = getUserAnimals($user_info['userId']);
-            foreach ($animals as $animal){
-                draw_animal($animal["petId"],$animal["name"],null,$animal["size"],$animal["color"],$animal["location"],null,$user);
-            }
-            ?>
-        </section>
-
-        <br>
-        <div>
-            <label>Favourites</label>
-        </div>
-
-        <section id="user_favourite_animals">
-            <?php
-            $animals = getUserFavouriteAnimals($user_info['userName']);
-            foreach ($animals as $animal){
-                draw_animal($animal["pet"],$animal["name"],null,$animal["size"],$animal["color"],$animal["location"],null,$animal['userName']);
-            }
-            ?>
-        </section>
+include_once '../templates/tpl_main.php';
+function draw_user_aside($user){
+    $user_info =  getUser($user);
+    ?>
+    <aside id="user_profile">
+        <img src = "<?=getPicturePath($user)?>" width="200" height="200"/>
+        <label>
+            <?=$user?>
+        </label>
+        <label>
+            <?=$user_info['Name']?>
+        </label>
+        <label>
+            <?=$user_info['EmailAddress']?>
+        </label>
         <?php
-            
-        
-    }
+        if($_SESSION['user']==$user){
+            ?>
+            <button id="edit_user_profile">Edit Profile</button>
+            <?php
+        }
+        ?>
+    </aside>
+
+    <div>
+        <label>User Animals</label>
+    </div>
+    <section id="user_animals">
+        <?php
+        $animals = getUserAnimals($user_info['userId']);
+        foreach ($animals as $animal){
+            draw_animal($animal["petId"],$animal["name"],null,$animal["size"],$animal["color"],$animal["location"],null,$user);
+        }
+        ?>
+    </section>
+
+    <br>
+    <div>
+        <label>Favourites</label>
+    </div>
+
+    <section id="user_favourite_animals">
+        <?php
+        $animals = getUserFavouriteAnimals($user_info['userName']);
+        foreach ($animals as $animal){
+            draw_animal($animal["pet"],$animal["name"],null,$animal["size"],$animal["color"],$animal["location"],null,$animal['userName']);
+        }
+        ?>
+    </section>
+    <?php
+
+
+}
