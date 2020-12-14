ccc818935ff9b73c0c199bebe5638be9584482b1
rocas777 <lmiguelapinto@gmail.com>
2020-12-14 16:51:54 +0000

    fixed error of user information depending os session user instead of page user

--- src/templates/tpl_user_profile.php
+++ src/templates/tpl_user_profile.php
@@ -3,7 +3,7 @@ include_once '../templates/tpl_main.php';
 function draw_user_aside($user)
 {
     $user_info =  getUser($user);
-?>
+    ?>
     <aside id="user_profile">
         <img src="<?= getPicturePath($user) ?>" width="200" height="200" />
         <label id="user">
@@ -17,11 +17,11 @@ function draw_user_aside($user)
         </label>
         <?php
         if (isset($_SESSION['user']) && $_SESSION['user'] == $user) {
-        ?>
+            ?>
             <script src="../js/user_editing.js" defer></script>
             <button id="edit_user_profile">Edit Profile</button>
             <a href="edit_pass.php">Change Password</a>
-        <?php
+            <?php
         }
         ?>
 
@@ -52,54 +52,60 @@ function draw_user_aside($user)
         }
         ?>
     </section>
-    
-    <br>
-    <div>
-        <label>Proposals</label>
-    </div>
-    <section id="user_proposals">
-        <?php
-        $user = getUser($_SESSION['user']);
-        $proposals = get_proposals_for_user($user['userId']);
-        if ($proposals) {
-            foreach ($proposals as $proposal) {
-                $pet = get_animal_data($proposal['pet']);
-                draw_proposal($_SESSION['user'], $pet['name'], $proposal['text'], $proposal['state']);
-            }
-        }
-        else{
-            echo "<label>You haven't made any proposals</label>";
-        }
-        ?>
-    </section>
 
     <br>
-    <div>
-        <label>Posts where you asked questions and/or replied</label>
-    </div>
-    <section id="user_posts">
-        <?php
-        $user = getUser($_SESSION['user']);
-        $questions = get_questions_participated($user['userId']);
-        if ($questions) {
-            foreach ($questions as $question) { ?>
-                <a class="animal_main_page" href = "animal_profile.php?pet_id=<?=$question['pet']?>"  >
-                    <div class="animal_img_box">
-                        <img class= "animal_image_box" src="<?=get_animal_photo($question['pet'])?>" width="200" height="200">
-                        <label class="question_text_box">
-                            <?=$question['questionTxt']?>
-                        </label>
-                    </div>
-                </a>
+    <?php
+    if(isset($_SESSION['user']) && $user===$_SESSION['user']) {
+        ?>
+        <div>
+            <label>Proposals</label>
+        </div>
+        <section id="user_proposals">
             <?php
+            $user = getUser($_SESSION['user']);
+            $proposals = get_proposals_for_user($user['userId']);
+            if ($proposals) {
+                foreach ($proposals as $proposal) {
+                    $pet = get_animal_data($proposal['pet']);
+                    draw_proposal($_SESSION['user'], $pet['name'], $proposal['text'], $proposal['state']);
+                }
+            } else {
+                echo "<label>You haven't made any proposals</label>";
             }
-        }
-        else{
-            echo "<label>You haven't made any questions/replies</label>";
-        }
-        ?>
-    </section>
-<?php
+            ?>
+        </section>
 
+        <br>
+        <?php
+    }
+    if(isset($_SESSION['user']) && $user===$_SESSION['user']) {
+        ?>
+        <div>
+            <label>Posts where you asked questions and/or replied</label>
+        </div>
+        <section id="user_posts">
+            <?php
+            $user = getUser($_SESSION['user']);
+            $questions = get_questions_participated($user['userId']);
+            if ($questions) {
+                foreach ($questions as $question) { ?>
+                    <a class="animal_main_page" href = "animal_profile.php?pet_id=<?=$question['pet']?>"  >
+                        <div class="animal_img_box">
+                            <img class= "animal_image_box" src="<?=get_animal_photo($question['pet'])?>" width="200" height="200">
+                            <label class="question_text_box">
+                                <?=$question['questionTxt']?>
+                            </label>
+                        </div>
+                    </a>
+                    <?php
+                }
+            }
+            else{
+                echo "<label>You haven't made any questions/replies</label>";
+            }
+            ?>
+        </section>
+        <?php
+    }
 
 }
